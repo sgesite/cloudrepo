b8b43a217229368748f3fef61b1c32524d4a3e84
sgesite <125855344+sgesite@users.noreply.github.com>
2023-02-20 19:06:18 +0800

    Delete GesiteEx1 directory

--- GesiteEx1/src/org/ssglobal/training/codes/itemA/MyClass.java
+++
@@ -1,8 +0,0 @@
-package org.ssglobal.training.codes.itemA;
-
-public class MyClass {
-	public static void main(String[] args) {
-		int x = 1; 
-		int y = 2;
-	}
-}
--- GesiteEx1/src/org/ssglobal/training/codes/itemD/JavaID.java
+++
@@ -1,9 +0,0 @@
-package org.ssglobal.training.codes.itemD;
-
-public class JavaID {
-	public String google;
-	public double $money;// _ and $ is not allowed anymore
-	public double money$;
-	public double $$money;// _ and $ is not allowed anymore
-	public String _piece; // _ and $ is not allowed anymore
-}
